b06765b55b5346fe8eca85024b83da2c04737791
suzukikoutarou <kotarosz727@gmail.com>
2023-02-28 09:46:29 +0900

    feat: ユーザーのindex取得実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Hello world!</h1>
+  <ul>
+    <li v-for="user in users" :key="user.id">
+      {{ user.name }}
+    </li>
+  </ul>
 </template>
+
+
+<script setup lang="ts">
+import { useUserRepository } from "../repository/user_repository";
+const { index } = useUserRepository();
+
+const users = await index();
+</script>
---
+++ repository/repository.ts
@@ -0,0 +1,19 @@
+export interface Id  {
+    id: string
+}
+
+export interface Entity  {
+    id: Id;
+}
+
+export interface HttpResponse {
+    status: number;
+    data?: [];
+}
+export interface Repository<T extends Entity> {
+    index:() => Promise<T[]>;
+    create:(payload: T) => Promise<T>;
+    show:(id: Id) => Promise<T>;
+    update:(payload: T) => Promise<T>;
+    destroy:(id: Id) => Promise<HttpResponse>;
+}
